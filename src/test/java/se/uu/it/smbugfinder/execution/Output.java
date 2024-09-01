958d57b85f43f982c4c4c73b3c50aeef00005ccb
kritonios01 <kritonios@gmail.com>
2024-09-01 17:30:06 +0300

    Changes to incorporate the lombok dependency

--- src/test/java/se/uu/it/smbugfinder/execution/Output.java
+++ src/test/java/se/uu/it/smbugfinder/execution/Output.java
@@ -1,39 +1,16 @@
 package se.uu.it.smbugfinder.execution;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
+import lombok.Data;
+
+@Data
 public class Output {
-  @Override
-  public int hashCode() {
-      return Objects.hash(bugs);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (!(o instanceof Output)) return false;
-    Output output = (Output) o;
-    return this.getSet().equals(output.getSet());
-  }
-
-  @Override
-  public String toString() {
-    return "{Set='" + bugs + '\'' + "}";
-  }
-
-  private Set<OutputBug> bugs;
+  private final Set<OutputBug> bugs;
 
   public Output() {
     this.bugs = new HashSet<OutputBug>();
-  }
-
-  public Output(Set<OutputBug> bugs) {
-    this.bugs = bugs;
-  }
-
-  public Set<OutputBug> getSet() {
-    return this.bugs;
   }
 
   public void addBug(OutputBug bug) {
