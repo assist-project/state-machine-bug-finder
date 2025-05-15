79e3c0f6538168380210f620a54a542207cf1d27
kritonios01 <kritonios@gmail.com>
2025-05-15 21:20:22 +0300

    Simplify two functions

--- pattern-language/semantics.ml
+++ pattern-language/semantics.ml
@@ -1,3 +1,5 @@
+open Either
+open Option
 open Ast
 
 exception SemanticError of string
@@ -13,27 +15,23 @@ let check_func_body valuesA valuesB = function
       List.(for_all (fun (k, v) -> 
         mem k valuesA && mem v valuesB) l)
 
-let handle_field = function
-  | Field (k, vs) -> (k, vs)
-  | _ -> assert false
-
-let handle_message = function
+let handle_entry = function
+  | Field (k, vs) -> Left (k, vs)
   | Message (_, field) ->
-      (match field with
-      | Message_fields (_, v) -> v)
-  | _ -> assert false
-
+      match field with
+      | Message_fields (_, v) -> Right v
 
 
 let check_stmt map = function
 | Fields es -> 
     List.fold_left (fun map x -> 
-      let k, v = handle_field x in
+      let k, v = x |> handle_entry |> find_left |> get in
       Fields.add k v map) map es
 | Messages es ->
     let fields =
-      List.map handle_message es
-      |> List.for_all (fun x -> Fields.mem x map) in
+      es |> List.map handle_entry 
+         |> List.map find_right |> List.map get
+         |> List.for_all (fun x -> Fields.mem x map) in
     if fields then map else 
       raise (SemanticError "Found a message associated to a non-declared field")
 | Function (name, ty, body) -> 
@@ -51,7 +49,7 @@ let check_stmt map = function
 
 let check_program = function
   (fields, messages, functions) as ast->
-    let fields = Option.value fields ~default: (Fields []) in
-    let messages = Option.value messages ~default: (Messages []) in
+    let fields = value fields ~default: (Fields []) in
+    let messages = value messages ~default: (Messages []) in
     let _ = List.fold_left check_stmt Fields.empty (fields :: messages :: functions) in
     ast
