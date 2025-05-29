85930ebd61cae1a2fe923037c2e782f47500ae78
kritonios01 <kritonios@gmail.com>
2025-05-29 18:12:19 +0300

    Adjust integration tests

--- src/test/java/se/uu/it/smbugfinder/execution/CommandsExec.java
+++ src/test/java/se/uu/it/smbugfinder/execution/CommandsExec.java
@@ -5,10 +5,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.StringReader;
+import java.lang.StackWalker.Option;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.junit.Assert;
@@ -17,61 +19,94 @@ import net.automatalib.exception.FormatException;
 import se.uu.it.smbugfinder.Main;
 
 public class CommandsExec {
-  protected void assertCorrectOutput(Output expectedOutput, Output output) {
-    Assert.assertEquals(expectedOutput.getBugs(), output.getBugs());
-  }
-
-  // Parse the output produced by Main.main and store it in an output object
-  private static Output getBugs(ByteArrayOutputStream outputStream) throws IOException {
-    Output bugs = new Output();
-    String pattern = null, severity = null, trace = null, inputs = null;
-    BufferedReader reader = new BufferedReader(new StringReader(outputStream.toString(StandardCharsets.UTF_8)));
-    String line;
-
-    while ((line = reader.readLine()) != null) {
-      if (line.startsWith("Bug Pattern:"))
-        pattern = line.substring(line.indexOf(":") + 1).trim();
-
-      if (line.startsWith("Severity:"))
-        severity = line.substring(line.indexOf(":") + 1).trim();
-
-      if (line.startsWith("Trace:"))
-        trace = line.substring(line.indexOf(":") + 1).trim();
-
-      if (line.startsWith("Inputs:"))
-        inputs = line.substring(line.indexOf(":") + 1).trim();
-
-      if (Stream.of(pattern, severity, trace, inputs).allMatch(x -> x != null)) {
-        bugs.addBug(new OutputBug(pattern, severity, trace, inputs));
-        pattern = null; severity = null; trace = null; inputs = null;
-      }
-    }
-    return bugs;
-  }
-
-  // Adjust the output stream of Main.main so we can store it in a stream of our own
-  public static Output runCommand(String model, String patterns, boolean eo, String... os) throws IOException, FormatException, InterruptedException {
-    List<String> command = new ArrayList<>(Arrays.asList(
-      "-m", model,
-      "-c", patterns
-    ));
-
-    if (eo) {
-      command.add("-eo");
-      command.add("NO_RESP");
+    protected void assertCorrectOutput(Output expectedOutput, Output output) {
+        Assert.assertEquals(expectedOutput.getBugs(), output.getBugs());
     }
 
-    if (os.length == 1) {
-      command.add("-os");
-      command.add(os[0]);
+    // Parse the output produced by Main.main and store it in an output object
+    private static Output getBugs(ByteArrayOutputStream outputStream) throws IOException {
+        Output bugs = new Output();
+        String pattern = null, severity = null, trace = null, inputs = null;
+        BufferedReader reader = new BufferedReader(new StringReader(outputStream.toString(StandardCharsets.UTF_8)));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            if (line.startsWith("Bug Pattern:"))
+                pattern = line.substring(line.indexOf(":") + 1).trim();
+
+            if (line.startsWith("Severity:"))
+                severity = line.substring(line.indexOf(":") + 1).trim();
+
+            if (line.startsWith("Trace:"))
+                trace = line.substring(line.indexOf(":") + 1).trim();
+
+            if (line.startsWith("Inputs:"))
+                inputs = line.substring(line.indexOf(":") + 1).trim();
+
+            if (Stream.of(pattern, severity, trace, inputs).allMatch(x -> x != null)) {
+                bugs.addBug(new OutputBug(pattern, severity, trace, inputs));
+                pattern = null;
+                severity = null;
+                trace = null;
+                inputs = null;
+            }
+        }
+        return bugs;
     }
 
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); //create a stream to hold the output of main
-    PrintStream ps = new PrintStream(outputStream);
-    System.setOut(ps); //redirect sysout to the new printstream
-    Main.main(command.toArray(new String[0]));
-    System.setOut(System.out); //restore the original sysout
+    // Adjust the output stream of Main.main so we can store it in a stream of our own
+    // public static Output runCommand(String model, String patterns) 
+    //         throws IOException, FormatException, InterruptedException {
+    //     return runCommand(model, patterns, null, false, null);
+    // }
 
-    return getBugs(outputStream);
-  }
+    // public static Output runCommand(String model, String patterns, String parameters) 
+    //         throws IOException, FormatException, InterruptedException {
+    //     return runCommand(model, patterns, parameters, false, null);
+    // }
+
+    public static Output runCommand(String model, String patterns, String parameters, String os) 
+            throws IOException, FormatException, InterruptedException {
+        return runCommand(model, patterns, parameters, false, os);
+    }
+
+    public static Output runCommand(String model, String patterns, String os) 
+            throws IOException, FormatException, InterruptedException {
+        return runCommand(model, patterns, null, false, os);
+    }
+
+    public static Output runCommand(String model, String patterns, boolean eo) 
+            throws IOException, FormatException, InterruptedException {
+        return runCommand(model, patterns, null, eo, null);
+    }
+
+    private static Output runCommand(String model, String patterns, String parameters, boolean eo, String os)
+            throws IOException, FormatException, InterruptedException {
+        List<String> command = new ArrayList<>(Arrays.asList(
+            "-m", model,
+            "-c", patterns));
+
+        if (parameters != null) {
+            command.add("-p");
+            command.add(parameters);
+        }
+
+        if (eo) {
+            command.add("-eo");
+            command.add("NO_RESP");
+        }
+
+        if (os != null) {
+            command.add("-os");
+            command.add(os);
+        }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(outputStream);
+        System.setOut(ps);
+        Main.main(command.toArray(new String[0]));
+        System.setOut(System.out);
+
+        return getBugs(outputStream);
+    }
 }
--- src/test/java/se/uu/it/smbugfinder/execution/DtlsCommandsExecIT.java
+++ src/test/java/se/uu/it/smbugfinder/execution/DtlsCommandsExecIT.java
@@ -1,31 +1,34 @@
 package se.uu.it.smbugfinder.execution;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.junit.Test;
 
 import net.automatalib.exception.FormatException;
 
-
 public class DtlsCommandsExecIT extends CommandsExec {
-  private static final String DTLS_SERVER_MODEL = "src/main/resources/models/dtls/server/MbedTLS-2.26.0_server_all_cert_req.dot";
-  private static final String DTLS_SERVER_BUG_PATTERNS = "src/main/resources/patterns/dtls/server/patterns.xml";
-  private static final String DTLS_CLIENT_MODEL = "src/main/resources/models/dtls/client/MbedTLS-2.26.0_client_psk_reneg.dot";
-  private static final String DTLS_CLIENT_BUG_PATTERNS = "src/main/resources/patterns/dtls/client/patterns.xml";
+    private static final String DTLS_SERVER_MODEL = "src/main/resources/models/dtls/server/MbedTLS-2.26.0_server_all_cert_req.dot";
+    private static final String DTLS_SERVER_BUG_PATTERNS = "src/main/resources/patterns/dtls/server/patterns.xml";
+    private static final String DTLS_CLIENT_MODEL = "src/main/resources/models/dtls/client/MbedTLS-2.26.0_client_psk_reneg.dot";
+    private static final String DTLS_CLIENT_BUG_PATTERNS = "src/main/resources/patterns/dtls/client/patterns.xml";
 
-  @Test
-  public void testDtlsServer () throws IOException, FormatException, InterruptedException {
-    Output expectedOutput = new Output();
-    expectedOutput.addBug(new OutputBug("Non-conforming Cookie", "LOW", "PSK_CLIENT_HELLO/HELLO_VERIFY_REQUEST RSA_CLIENT_HELLO/SERVER_HELLO", "PSK_CLIENT_HELLO RSA_CLIENT_HELLO"));
-    Output output = CommandsExec.runCommand(DTLS_SERVER_MODEL, DTLS_SERVER_BUG_PATTERNS, false, "|");
-    assertCorrectOutput(expectedOutput, output);
-  }
+    @Test
+    public void testDtlsServer() throws IOException, FormatException, InterruptedException {
+        Output expectedOutput = new Output();
+        expectedOutput.addBug(new OutputBug("Non-conforming Cookie", "LOW",
+                "PSK_CLIENT_HELLO/HELLO_VERIFY_REQUEST RSA_CLIENT_HELLO/SERVER_HELLO",
+                "PSK_CLIENT_HELLO RSA_CLIENT_HELLO"));
+        Output output = CommandsExec.runCommand(DTLS_SERVER_MODEL, DTLS_SERVER_BUG_PATTERNS, "|");
+        assertCorrectOutput(expectedOutput, output);
+    }
 
-  @Test
-  public void testDtlsClient () throws IOException, FormatException, InterruptedException {
-    Output expectedOutput = new Output();
-    expectedOutput.addBug(new OutputBug("Premature HelloRequest", "LOW", "HELLO_REQUEST/CLIENT_HELLO", "HELLO_REQUEST"));
-    Output output = CommandsExec.runCommand(DTLS_CLIENT_MODEL, DTLS_CLIENT_BUG_PATTERNS, false, "|");
-    assertCorrectOutput(expectedOutput, output);
-  }
+    @Test
+    public void testDtlsClient() throws IOException, FormatException, InterruptedException {
+        Output expectedOutput = new Output();
+        expectedOutput
+                .addBug(new OutputBug("Premature HelloRequest", "LOW", "HELLO_REQUEST/CLIENT_HELLO", "HELLO_REQUEST"));
+        Output output = CommandsExec.runCommand(DTLS_CLIENT_MODEL, DTLS_CLIENT_BUG_PATTERNS, "|");
+        assertCorrectOutput(expectedOutput, output);
+    }
 }
